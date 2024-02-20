510ca685535ea1f1c0441726119dd5686775d1c4
nbanho <nicbanjo@gmail.com>
2024-02-20 11:58:39 +0100

    Revised temporal model

--- models/tm-v1.R
+++ models/tm-v1.R
@@ -1,47 +1,46 @@
 #' Spatiotemporal model of the quanta concentration
-#' 
-#' @description Function to compute the quanta concentration at each time point. 
-#' 
+#'
+#' @description Function to compute the quanta concentration at each time point.
+#'
 #' @param time time vector in 1s
 #' @param c0 2d-matrix with initial quanta concentration (in quanta per m3)
 #' @param inf data frame with time, x- and y-coordinates, and generated quanta q
 #' @param aer air changes per second
 #' @param lambda viral inactivation rate per second
 #' @param deposit deposition rate per second due to gravitational settling
-#' 
-#' @details The default for parameters q, lambda, and k is based on prior research for Mtb. 
+#'
+#' @details The default for parameters q, lambda, and k is based on prior research for Mtb.
 #' The diffusion constant is computed relative to the air change rate, see Cheng (2011).
-#' 
-#' @references Cheng KC, Acevedo-Bolton V, Jiang RT, Klepeis NE, Ott WR, Fringer OB, Hildemann LM. 
-#' Modeling exposure close to air pollution sources in naturally ventilated residences: 
-#' Association of turbulent diffusion coefficient with air change rate. 
-#' Environmental science & technology. 2011;45(9):4016-22. 
-#' 
-#' @return a list of matrices with the quanta concentration at each time step t_i   
-#' 
+#'
+#' @references Cheng KC, Acevedo-Bolton V, Jiang RT, Klepeis NE, Ott WR, Fringer OB, Hildemann LM.
+#' Modeling exposure close to air pollution sources in naturally ventilated residences:
+#' Association of turbulent diffusion coefficient with air change rate.
+#' Environmental science & technology. 2011;45(9):4016-22.
+#'
+#' @return a list of matrices with the quanta concentration at each time step t_i
+#'
 
-tm <- function(time, 
-               c0, 
-               inf, 
-               aer = 3/3600,
-               lambda = 1/3600,
+tm <- function(time,
+               c0,
+               inf,
+               aer = 3 / 3600,
+               lambda = 1 / 3600,
                deposit = 0) {
-  
   # room dimensions
   nr <- nrow(c0)
   nc <- ncol(c0)
   n_cells <- nr * nc
-  
-  # initialize 
+
+  # initialize
   nT <- length(time)
   time_int <- 1:nT
-  ct <- replicate(nT+2, c0)
+  ct <- replicate(nT + 2, c0)
   x_crds <- row.names(c0)
   y_crds <- colnames(c0)
-  
+
   # add start and end without quanta generation
   if (nrow(inf) == 0 & sum(c0) == 0) {
-    return(ct[,,-1])
+    return(ct[, , -1])
   } else if (nrow(inf) == 0) {
     inf <- tibble(t = 1, data = list(data.frame(x = y_crds[1], y = x_crds[1], q = 0)))
     inf$te <- max(time_int) + 2
@@ -56,30 +55,27 @@ tm <- function(time,
     inf$te <- c(inf$t[-1], max(time_int) + 2)
   }
   inf$dt <- inf$te - inf$t
-  
+
   # total consumption rate
-  r <- -(aer + lambda + deposit)
-  
+  r <- aer + lambda + deposit
+
   for (k in 1:nrow(inf)) {
-    
     # time point
     t_k <- inf$t[k]
-    te_k <- inf$te[k] 
+    te_k <- inf$te[k]
     dt_k <- inf$dt[k]
-    
+
     # previous quanta
-    ct[,,t_k+1] <- ct[,,t_k]
-    
+    ct[, , t_k + 1] <- ct[, , t_k]
+
     # quanta generation
     for (i in 1:nrow(inf$data[[k]])) {
-      ct[yt:yb,xl:xr,t_k+1] <- ct[yt:yb,xl:xr,t_k+1] + inf$data[[k]]$q[i] / n_cells
+      ct[, , t_k + 1] <- ct[, , t_k + 1] + inf$data[[k]]$q[i] / n_cells
     }
-    
+
     # removal
-    ct[,,(t_k+1):(te_k)] <- ct[yt:yb,xl:xr,t_k+1] * exp(-r * 1:dt_k)
-    
+    ct[, , (t_k + 1):(te_k)] <- sapply(1:dt_k, function(dt_k_i) ct[, , t_k + 1] * exp(-r * dt_k_i), simplify = "array")
   }
-  
-  return(ct[,,-c(1,nT+2)])
-  
+
+  return(ct[, , -c(1, nT + 2)])
 }
--- preprocessing/simulate_qctr.R
+++ preprocessing/simulate_qctr.R
@@ -1,7 +1,7 @@
 #### Command line ###
 
 # all command line arguments
-args <- commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 
 # start simulation
 #' e.g. if the previous simulation was interrupted
@@ -30,12 +30,12 @@ cellSize <- as.numeric(args[6]) # in px
 #' without mask wearing
 #' 0: without mask-wearing
 #' 1: with masking
-is_masking <- as.numeric(args[7]) 
+is_masking <- as.numeric(args[7])
 
 #' fixed air chnage rate
 #' >0: fixed AER replacing empirical one(s)
 #' 0: use empirical one(s)
-fixed_aer <- as.numeric(args[8]) 
+fixed_aer <- as.numeric(args[8])
 
 #' population-level prevalence
 #' du: use diagnosed and undiagnosed TB patients
@@ -93,11 +93,12 @@ library(lubridate)
 source("utils/spatial.r")
 source("utils/trans_risk.r")
 source("models/stm-v2.R")
+source("models/tm-v1.R")
 
 
 #### Time ####
 
-date <- paste0("2021-",mth, "-", dy)
+date <- paste0("2021-", mth, "-", dy)
 t_start <- as.POSIXct(paste(date, "08:00:00"))
 midday <- as.POSIXct(paste(date, "12:00:00"))
 t_end <- as.POSIXct(paste(date, "16:00:00"))
@@ -115,13 +116,10 @@ source("preprocessing/prep_building-rasterize.R")
 #### Tracking data ####
 
 if (file.exists(track_file)) { # check if file with that cell size already exists
-  
+
   track <- readRDS(track_file)
-  
 } else {
-  
   stop("Track file does not exist.")
-  
 }
 
 
@@ -131,38 +129,33 @@ track_undiag <- track %>%
   dplyr::filter(tracking_end != "Possible HCW") # always consider HCW as non-infectious
 
 if (who_is_tb == "du") {
-  
   # diagnosed
   track_diag <- dplyr::filter(track, is_infectious)
-  
+
   # undiagnosed
   track_undiag <- dplyr::filter(track_undiag, !is_infectious | is.na(is_infectious))
-  
 } else if (who_is_tb == "ds") {
-  
   # diagnosed and suspected
   track_diag <- dplyr::filter(track, is_infectious | was_suspected)
   pid_diag <- unique(track_diag$patient_id)
-  
+
   # no undiagnosed/unsuspected TB patients
   track_undiag <- dplyr::filter(track, patient_id == -1)
-  
-} else if(who_is_tb == "pu") {
-  
+} else if (who_is_tb == "pu") {
   # no diagnosed TB patients
   track_diag <- dplyr::filter(track, patient_id == -1)
-  
+
   # only undiagnosed TB patients
   # track_undiag <- track_undiag
-  
-  
 }
 
 
 #### Environmental data #####
 aer_df <- readRDS("data-clean/environmental/air-change-rate.rds") %>%
-  filter(month(date) == as.numeric(mth),
-         day(date) == as.numeric(dy)) 
+  filter(
+    month(date) == as.numeric(mth),
+    day(date) == as.numeric(dy)
+  )
 
 if (fixed_aer > 0) {
   aer_df$aer <- fixed_aer
@@ -172,180 +165,183 @@ if (fixed_aer > 0) {
 #### Assumptions ####
 
 if (file.exists(param_file)) {
-  
   sim_param <- readRDS(param_file)
-  
 } else {
-  
   stop("Parameter file does not exist.")
-  
 }
 
 
 for (sim in cont_n:term_n) {
-  
   print(paste0("Sim: ", sim))
-  
+
   # TB patient tracks
   track_tb_all <- rbind(
     track_diag,
-    filter(track_undiag, patient_id %in% sim_param[[paste0("undiagTB","_", who_is_tb)]][[sim]])
+    filter(track_undiag, patient_id %in% sim_param[[paste0("undiagTB", "_", who_is_tb)]][[sim]])
   )
-  
+
   # no TB patient tracks but including HCW
   track_susc_all <- rbind(
     filter(track, tracking_end == "Possible HCW"),
-    filter(track_undiag, ! (patient_id %in% sim_param[[paste0("undiagTB","_", who_is_tb)]][[sim]]))
+    filter(track_undiag, !(patient_id %in% sim_param[[paste0("undiagTB", "_", who_is_tb)]][[sim]]))
   )
-  
+
   # initialize
   susc_quanta_exp <- list()
-  
+
   for (room in sel_rooms) {
-    
     print(paste0("- ", room))
-    
+
     # room setup
     if (room == "Waiting room") {
-      
       # initial quanta concentration matrix
       c0 <- sP_to_matrix(waiting_room)
-      
+
       # (no) TB patients in room
       track_tb <- filter(track_tb_all, is_waitingroom)
       track_susc <- filter(track_susc_all, is_waitingroom)
-      
+
       # room dimension
       roomDim <- dimWR
-      
     } else if (room == "Corridor") {
-      
       # initial quanta concentration matrix
       c0 <- sP_to_matrix(corridor)
-      
+
       # TB patients in room
       track_tb <- filter(track_tb_all, is_passage)
       track_susc <- filter(track_susc_all, is_passage)
-      
+
       # room dimension
       roomDim <- dimCD
-      
     } else if (room == "TB room") {
-      
       # initial quanta concentration matrix
       c0 <- sP_to_matrix(tb_room)
-      
+
       # TB patients in room
       track_tb <- filter(track_tb_all, is_tbroom)
       track_susc <- filter(track_susc_all, is_tbroom)
-      
+
       # room dimension
       roomDim <- dimTB
-      
     } else {
       stop("Error: Room invalid.")
     }
-    
+
     # add quanta generation rate per second by activity level
     track_tb <- track_tb %>%
       dplyr::select(time, cell_x, cell_y, activity) %>%
       rename(x = cell_x, y = cell_y) %>%
-      mutate(q = ifelse(activity == 1, sim_param$q_wait[sim_param$sim == sim], sim_param$q_walk[sim_param$sim == sim]),
-             q = ifelse(is_masking == 1, q * sim_param$mask_red[sim_param$sim == sim], q),
-             q = q / 3600)
-    
+      mutate(
+        q = ifelse(activity == 1, sim_param$q_wait[sim_param$sim == sim], sim_param$q_walk[sim_param$sim == sim]),
+        q = ifelse(is_masking == 1, q * sim_param$mask_red[sim_param$sim == sim], q),
+        q = q / 3600
+      )
+
     # quanta conc in the morning
     print("-- morning")
     track_tb_morn <- filter(track_tb, time <= midday)
-    track_tb_morn$t <- as.numeric(difftime(track_tb_morn$time, t_start, units = "sec")) 
-    
+    track_tb_morn$t <- as.numeric(difftime(track_tb_morn$time, t_start, units = "sec"))
+
     if (mod == "spattemp") {
-  
-      ct_morn <- stm(time = tt_morn,
-                     c0 = c0, 
-                     inf = dplyr::select(track_tb_morn, -time, -activity),
-                     cellLength = convert_dist(cellSize),
-                     pd = ifelse(is_masking == 1, 0, 1),
-                     vol = prod(roomDim),
-                     aer = aer_df$aer[aer_df$room==room & aer_df$daytime=="Morning"] / 3600,
-                     lambda = sim_param$viral_inact[sim_param$sim == sim] / 3600,
-                     deposit = 0)
-      
+      ct_morn <- stm(
+        time = tt_morn,
+        c0 = c0,
+        inf = dplyr::select(track_tb_morn, -time, -activity),
+        cellLength = convert_dist(cellSize),
+        pd = ifelse(is_masking == 1, 0, 1),
+        vol = prod(roomDim),
+        aer = aer_df$aer[aer_df$room == room & aer_df$daytime == "Morning"] / 3600,
+        lambda = sim_param$viral_inact[sim_param$sim == sim] / 3600,
+        deposit = 0
+      )
     } else if (mod == "temp") {
-      
-      stop("Model needs to be revised")
-      
+      ct_morn <- tm(
+        time = tt_morn,
+        c0 = c0,
+        inf = dplyr::select(track_tb_morn, -time, -activity),
+        aer = aer_df$aer[aer_df$room == room & aer_df$daytime == "Morning"] / 3600,
+        lambda = sim_param$viral_inact[sim_param$sim == sim] / 3600,
+        deposit = 0
+      )
     } else {
-      
       stop("Model does not exist")
-      
     }
-    
-    ct_morn_mean <- apply(ct_morn, c(1,2), mean)
+
+    ct_morn_mean <- apply(ct_morn, c(1, 2), mean)
     saveRDS(ct_morn_mean, paste0(sim_path_to_file, "mean_quanta_conc-morning-", room, "-sim-", sim, ".rds"))
-    
-    
+
+
     # quanta conc in the afternoon
     print("-- afternoon")
     if (max(track_susc$time) > midday) {
-      c0_noon <- ct_morn[,,dim(ct_morn)[3]]
+      c0_noon <- ct_morn[, , dim(ct_morn)[3]]
     } else {
       c0_noon <- c0
     }
     track_tb_noon <- filter(track_tb, time > midday)
-    track_tb_noon$t <- as.numeric(difftime(track_tb_noon$time, midday + lubridate::seconds(1), units = "sec")) 
-    
-      
+    track_tb_noon$t <- as.numeric(difftime(track_tb_noon$time, midday + lubridate::seconds(1), units = "sec"))
+
+
     if (mod == "spattemp") {
-      
-      ct_noon <- stm(time = tt_noon,
-                     c0 = c0_noon, 
-                     inf = dplyr::select(track_tb_noon, -time, -activity),
-                     cellLength = convert_dist(cellSize),
-                     pd = ifelse(is_masking == 1, 0, 1),
-                     vol = prod(roomDim),
-                     aer = aer_df$aer[aer_df$room==room & aer_df$daytime=="Afternoon"] / 3600,
-                     lambda = sim_param$viral_inact[sim_param$sim == sim] / 3600,
-                     deposit = 0)
-      
+      ct_noon <- stm(
+        time = tt_noon,
+        c0 = c0_noon,
+        inf = dplyr::select(track_tb_noon, -time, -activity),
+        cellLength = convert_dist(cellSize),
+        pd = ifelse(is_masking == 1, 0, 1),
+        vol = prod(roomDim),
+        aer = aer_df$aer[aer_df$room == room & aer_df$daytime == "Afternoon"] / 3600,
+        lambda = sim_param$viral_inact[sim_param$sim == sim] / 3600,
+        deposit = 0
+      )
     } else if (mod == "temp") {
-      
-      # TODO: revise
-      stop("Model needs to be revised")
-      
+      ct_noon <- tm(
+        time = tt_noon,
+        c0 = c0_noon,
+        inf = dplyr::select(track_tb_noon, -time, -activity),
+        aer = aer_df$aer[aer_df$room == room & aer_df$daytime == "Afternoon"] / 3600,
+        lambda = sim_param$viral_inact[sim_param$sim == sim] / 3600,
+        deposit = 0
+      )
     } else {
-      
       stop("Model does not exist")
-      
     }
-    
-    ct_noon_mean <- apply(ct_noon, c(1,2), mean)
+
+    ct_noon_mean <- apply(ct_noon, c(1, 2), mean)
     saveRDS(ct_noon_mean, paste0(sim_path_to_file, "mean_quanta_conc-afternoon-", room, "-sim-", sim, ".rds"))
 
-    
+
     # compute quanta conc exposure by susc
     ct <- abind::abind(ct_morn, ct_noon, along = 3)
     if (nrow(track_susc) > 0) {
-      track_susc$quanta <- mapply(function(t, x, y) {if (t == 0) 0 else {ct[y,x,t]}}, 
-                                  t=track_susc$t, x=as.character(track_susc$cell_x), y=as.character(track_susc$cell_y))
+      track_susc$quanta <- mapply(
+        function(t, x, y) {
+          if (t == 0) {
+            0
+          } else {
+            ct[y, x, t]
+          }
+        },
+        t = track_susc$t, x = as.character(track_susc$cell_x), y = as.character(track_susc$cell_y)
+      )
       track_susc <- track_susc %>%
-        mutate(quanta_conc = inhal_rate * quanta / (convert_dist(cellSize) ^ 2 * convert_dist(cell_height))) %>%
+        mutate(quanta_conc = inhal_rate * quanta / (convert_dist(cellSize)^2 * convert_dist(cell_height))) %>%
         dplyr::select(patient_id, quanta_conc)
     } else {
       track_susc <- track_susc %>%
         mutate(quanta_conc = numeric()) %>%
         dplyr::select(patient_id, quanta_conc)
     }
-   
+
     susc_quanta_exp[[room]] <- track_susc
   }
-  
+
   # compute transmission risk per susceptible
   trans_risk <- do.call(rbind, susc_quanta_exp) %>%
     group_by(patient_id) %>%
     summarize(quanta_conc = sum(quanta_conc)) %>%
     ungroup() %>%
-    mutate(P = 1 - exp(-quanta_conc)) 
-  
+    mutate(P = 1 - exp(-quanta_conc))
+
   saveRDS(trans_risk, paste0(sim_path_to_file, "trans-risk-", sim, ".rds"))
 }
