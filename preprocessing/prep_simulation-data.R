408aed950e2840ece757d0375719aaab51e1ab44
nbanho <nicbanjo@gmail.com>
2024-03-07 15:54:47 +0100

    Modeling fixes and visual updates

--- preprocessing/prep_simulation-data.R
+++ preprocessing/prep_simulation-data.R
@@ -42,10 +42,9 @@ ncores <- 6
 #### Track data ####
 
 for (i in 1:length(dates)) {
-  
   # date
   d <- dates[i]
-  
+
   # daytime
   dytm <- daytimes[i]
   t_start <- as.POSIXct(paste(d, "08:00:00"))
@@ -53,36 +52,38 @@ for (i in 1:length(dates)) {
   tt <- seq(t_start, t_end, by = "1 sec")
   midday <- as.POSIXct(paste(d, "12:00:00"))
   midday_idx <- which(tt == midday)
-  
+
   # existing track file
   track_file <- paste0(track_file_path, d, ".rds")
-  
+
   # tracking
   if (!file.exists(track_file)) {
-    
     # load data
     track <- readRDS(paste0("data-clean/patient-tracking/matched-clinical/", d, ".rds")) %>%
       mutate(activity = ifelse(activity == "waiting", 1, 2))
-    
+
     # filter daytime
     if (dytm == "both") {
-      track <- filter(track, between(time, t_start, t_end)) 
+      track <- filter(track, between(time, t_start, t_end))
     } else if (dytm == "morning") {
-      track <- filter(track, between(time, t_start, midday)) 
+      track <- filter(track, between(time, t_start, midday))
     } else if (dytm == "afternoon") {
-      track <- filter(track, between(time, midday, t_end)) 
+      track <- filter(track, between(time, midday, t_end))
     } else {
       stop("Unknown daytime")
     }
-    
+
     # determine cell
     track$cell_id <- NA
-    track$cell_id[track$is_waitingroom] <- mcmapply(find_raster, track$x[track$is_waitingroom], track$y[track$is_waitingroom], 
-                                                    MoreArgs = list(cellCoordsXY = as.matrix(wrCoord[,c("x","y")])), mc.cores = ncores)
-    track$cell_id[track$is_passage] <- mcmapply(find_raster, track$x[track$is_passage], track$y[track$is_passage], 
-                                                MoreArgs = list(cellCoordsXY = as.matrix(cdCoord[,c("x","y")])), mc.cores = ncores)
-    track$cell_id[track$is_tbroom] <- mcmapply(find_raster, track$x[track$is_tbroom], track$y[track$is_tbroom], 
-                                               MoreArgs = list(cellCoordsXY = as.matrix(tbCoord[,c("x","y")])), mc.cores = ncores)
+    track$cell_id[track$is_waitingroom] <- mcmapply(find_raster, track$x[track$is_waitingroom], track$y[track$is_waitingroom],
+      MoreArgs = list(cellCoordsXY = as.matrix(wrCoord[, c("x", "y")])), mc.cores = ncores
+    )
+    track$cell_id[track$is_passage] <- mcmapply(find_raster, track$x[track$is_passage], track$y[track$is_passage],
+      MoreArgs = list(cellCoordsXY = as.matrix(cdCoord[, c("x", "y")])), mc.cores = ncores
+    )
+    track$cell_id[track$is_tbroom] <- mcmapply(find_raster, track$x[track$is_tbroom], track$y[track$is_tbroom],
+      MoreArgs = list(cellCoordsXY = as.matrix(tbCoord[, c("x", "y")])), mc.cores = ncores
+    )
     track$cell_id <- unlist(track$cell_id)
     track$cell_x[track$is_waitingroom] <- as.character(wrCoord$x[track$cell_id[track$is_waitingroom]])
     track$cell_x[track$is_passage] <- as.character(cdCoord$x[track$cell_id[track$is_passage]])
@@ -90,22 +91,25 @@ for (i in 1:length(dates)) {
     track$cell_y[track$is_waitingroom] <- as.character(wrCoord$y[track$cell_id[track$is_waitingroom]])
     track$cell_y[track$is_passage] <- as.character(cdCoord$y[track$cell_id[track$is_passage]])
     track$cell_y[track$is_tbroom] <- as.character(tbCoord$y[track$cell_id[track$is_tbroom]])
-    
+
     # determine time
-    track$t <- as.numeric(difftime(track$time, t_start, units = "sec")) 
-    
+    track$t <- as.numeric(difftime(track$time, t_start, units = "sec"))
+
     # cell height
     track$cell_height <- ifelse(track$is_waitingroom, dimWR[3],
-                                ifelse(track$is_passage, dimCD[3], dimTB[3]))
-    track$cell_height <- 1/convert_dist(1) * track$cell_height
-    
+      ifelse(track$is_passage, dimCD[3], dimTB[3])
+    )
+    track$cell_height <- 1 / convert_dist(1) * track$cell_height
+
     # inhalation rate depending on activity
-    track$inhal_rate <- ifelse(track$activity == 1, .5 * IR("sitting", "male") + .5 * IR("sitting","female"),
-                               .5 * IR("walking", "male") + .5 * IR("walking","female"))
-    
+    track$inhal_rate <- ifelse(track$activity == 1,
+      .5 * IR("sitting", "male") + .5 * IR("sitting", "female"),
+      .5 * IR("walking", "male") + .5 * IR("walking", "female")
+    )
+    track$inhal_rate <- track$inhal_rate / 3600
+
     # save
     saveRDS(track, file = track_file)
-    
   }
 }
 
@@ -114,44 +118,36 @@ for (i in 1:length(dates)) {
 #### Assumptions ####
 
 for (d in dates) {
-  
   # track file
-  track_file <- paste0(track_file_path, d, ".rds") 
-  
+  track_file <- paste0(track_file_path, d, ".rds")
+
   if (file.exists(track_file)) {
-    
     # load data
-    track <- readRDS(track_file) 
-    
+    track <- readRDS(track_file)
   } else {
-    
     stop("Track file not available.")
-    
   }
-  
+
   # set seed
   set.seed(seed)
-  
+
   # parameter file
   param_file <- paste0("simulations/", "/", "sim-param-", d, ".rds")
-  
-  if (file.exists(param_file)) { 
-    
-    sim_param <- readRDS(param_file) 
-    
+
+  if (file.exists(param_file)) {
+    sim_param <- readRDS(param_file)
   } else {
-    
     # quanta generation rate: bacillary load
     cb <- bact_load.mtb(n)
     q_wait <- sapply(cb, rq, activity = "waiting", breathing_weight = .8)
-    q_walk = sapply(cb, rq, activity = "walking", breathing_weight = .8)
-    
+    q_walk <- sapply(cb, rq, activity = "walking", breathing_weight = .8)
+
     # mask wearing reduction
     q_masking_red <- rrmask(n)
-    
+
     # viral inactivation rate
     lambda <- rlambda(n)
-    
+
     # combine into tibble
     sim_param <- tibble(
       sim = 1:n,
@@ -160,62 +156,53 @@ for (d in dates) {
       mask_red = q_masking_red,
       viral_inact = lambda
     )
-    
   }
-  
+
   for (w in who_is_tb) {
-    
     # split diagnosed and undiagnosed TB patients
     track_undiag <- track %>%
       dplyr::filter(tracking_end != "Possible HCW") # always consider HCW as non-infectious
-    
+
     if (w == "du") {
-      
       # undiagnosed
       track_undiag <- dplyr::filter(track_undiag, !is_infectious | is.na(is_infectious))
       pid_undiag <- unique(track_undiag$patient_id)
-      
+
       # number of undiagnosed TB patients
       n_undiag <- rundiag(n)
-      
+
       # IDs of undiagnosed TB patients
       undiag_TB <- lapply(n_undiag, sample, x = pid_undiag, replace = F)
-      
+
       # add to tibble
       sim_param <- sim_param %>%
         mutate(undiagTB_du = undiag_TB)
-      
     } else if (w == "ds") {
-      
       # no undiagnosed/unsuspected TB patients
       track_undiag <- dplyr::filter(track, patient_id == -1)
       pid_undiag <- -1
-      
+
       # no undiagnosed TB patients
       undiag_TB <- as.list(replicate(n, as.integer(-1)))
-      
+
       sim_param <- sim_param %>%
         mutate(undiagTB_ds = undiag_TB)
-      
     } else if (w == "pu") {
-      
       # only undiagnosed TB patients
       # track_undiag <- track_undiag
       pid_undiag <- unique(track_undiag$patient_id)
-      
+
       # number of undiagnosed TB patients
       p_undiag <- rundiag(n, empirical = F)
-      
+
       # IDs of undiagnosed TB patients
       undiag_TB <- lapply(p_undiag, function(p) pid_undiag[rbinom(length(pid_undiag), size = 1, prob = p) == 1])
-      
+
       sim_param <- sim_param %>%
         mutate(undiagTB_pu = undiag_TB)
-      
     }
-    
+
     # save tibble
     saveRDS(sim_param, file = param_file)
-    
   }
 }
--- utils/trans_risk.r
+++ utils/trans_risk.r
@@ -1,26 +1,26 @@
 #' Number of undiagnosed TB patients
-#' 
-#' @param n number of samples 
+#'
+#' @param n number of samples
 #' @param empirical sample based on empirical counts (default `True`) or population-level prevalence
-#' @param counts number of counts of TB patients (0, 1, 2, ...) vising the clinic  
+#' @param counts number of counts of TB patients (0, 1, 2, ...) vising the clinic
 #' @param prev_est prevalence estimates in %, either a vector with mean and sd, or a ISO-3 char for the country (if estimates are available)
-#' 
-#' @details 
+#'
+#' @details
 #' Distribution parametrized empirically based on the counts of occurrences of the
 #' daily number of diagnosed TB patients visiting the clinic,
 #' using a multinomial distribution, and
 #' assuming that the number of undiagnosed TB patients is similar in number.
-#' 
-#' Population-level prevalence for South Africa taken from Moyo. 
-#' 
+#'
+#' Population-level prevalence for South Africa taken from Moyo.
+#'
 #' @return if empirical, number of undiagnosed TB patients, otherwise proportion of undiagnosed TB patients
-#' 
-#' @references 
+#'
+#' @references
 #' - Moyo: Moyo et al. (2022) in Lancet Infect Dis (doi.org/10.1016/S1473-3099(22)00149-9)
 
-rundiag <- function(n, empirical = T, counts = c(6, 6, 7, 4, 5, 1), prev_est = "RSA") {
+rundiag <- function(n, empirical = T, counts = c(6, 12, 8, 2, 1), prev_est = "RSA") {
   if (empirical) {
-    BM <- rmultinom(n, size = 1, prob = counts) 
+    BM <- rmultinom(n, size = 1, prob = counts)
     n_undiag <- apply(BM, 2, function(col) which(col == 1)) - 1
     return(n_undiag)
   } else {
@@ -42,123 +42,142 @@ rundiag <- function(n, empirical = T, counts = c(6, 6, 7, 4, 5, 1), prev_est = "
 
 
 #' Reduction in quanta emission rate through surgical mask wearing
-#' 
+#'
 #' @param n number of samples
-#' 
-#' @details 
+#'
+#' @details
 #' Distribution approximated based on the reported mean and confidence interval
-#' by Dharmadhikari for the reduction with face masks worn by patients with
+#' by McCreesh who references Dharmadhikari for the reduction with face masks worn by patients with
 #' multi-drug resistant TB.
-#' 
-#' @references 
+#'
+#' @references
+#' - McCreesh: MCreesh et al. (2021) BMJ Global Health (doi.org/10.1136/bmjgh-2021-007124)
 #' - Dharmadhikari: Dharmadhikari et al. (2012) in Am J Respir Crit Care Med (doi.org/10.1164/rccm.201107-1190OC)
+#'
+
+fit_beta <- function(mean, lower, upper) {
+  # Calculate the sample variance
+  variance <- ((upper - lower) / (qnorm(0.975) * 2))^2
+
+  # Calculate the parameters of the beta distribution
+  alpha <- ((1 - mean) / variance - 1 / mean) * mean^2
+  beta <- alpha * (1 / mean - 1)
+
+  return(c(alpha, beta))
+}
+
+rrmask_par <- fit_beta(.75, .56, .85)
 
 rrmask <- function(n) {
-  LaplacesDemon::rtrunc(n, spec = "norm", a = 0, b = 1, mean = 0.56, sd = 0.11)
+  rbeta(n, rrmask_par[1], rrmask_par[2])
 }
 
 
 #' Inhalation rate
-#' 
+#'
 #' @param activity one of `c("resting", "sitting", "standing", "walking")`
 #' @param sex one of `c("male", "female")`
-#' 
+#'
 #' @return inhalation rate in m3 per h
-#' 
-#' @details 
+#'
+#' @details
+#' See Summary Table 1 from Adam's paper.
 #' Walking corresponds to walking slowly at 2.5 mph (light activity).
-#' 
-#' @references 
+#'
+#' @references
 #' - Adams: Adams (1993) [Report] Measurement of Breathing Rate and Volume in Routinely Performed Daily Activities.
-#' 
+#'
 
 IR <- function(activity, sex) {
-  IR <- matrix(c(7.12, 8.98, 
-                 7.72, 9.30,
-                 8.36, 10.65,
-                 20.32, 24.13),
-               ncol = 2, byrow = T,
-               dimnames = list(c("resting", "sitting", "standing", "walking"), 
-                               c("male", "female")))
-  IR <- IR / 2 / 1000 * 60
-  return(IR[activity,sex])
+  IR <- matrix(
+    c(
+      7.12, 8.98,
+      7.72, 9.30,
+      8.36, 10.65,
+      20.32, 24.13
+    ),
+    ncol = 2, byrow = T,
+    dimnames = list(
+      c("resting", "sitting", "standing", "walking"),
+      c("male", "female")
+    )
+  )
+  IR <- IR / 1000 * 60
+  return(IR[activity, sex])
 }
 
 
 #' Quanta emission rate
-#' 
+#'
 #' @param viral_load RNA viral or CFU bacillary load
 #' @param cf conversion factor to quanta
 #' @param activity physical activity (either waiting/sitting or walking)
 #' @param breathing_weight proportion of breathing (other: speaking)
-#' 
-#' @details 
-#' Mikszewski estimates the quanta generation rate distribution using 
-#' the inhalation rates (IR) for resting, standing, and light activity (slow walking) 
+#'
+#' @details
+#' Mikszewski estimates the quanta generation rate distribution using
+#' the inhalation rates (IR) for resting, standing, and light activity (slow walking)
 #' from Adams (1993) and computes the droplet volume emission rate VER as the product
-#' of the IR and the droplet volume concentration Vd, using the droplet 
+#' of the IR and the droplet volume concentration Vd, using the droplet
 #' emission rates from Stadnytskyi. Stadnytskyi estimates the transmission rates
-#' only for speaking, and (I assume) Mikszewski scales them to other respiratory 
-#' activities (breathing, loud speaking), using the multipliers derived 
-#' from the estimates for Vd from Morawska as done in Buonanno. 
-#' Accordingly, we also re-scale the VER reported by Mikszewski by multiplying them with 
+#' only for speaking, and (I assume) Mikszewski scales them to other respiratory
+#' activities (breathing, loud speaking), using the multipliers derived
+#' from the estimates for Vd from Morawska as done in Buonanno.
+#' Accordingly, we also re-scale the VER reported by Mikszewski by multiplying them with
 #' IR ratios for different physical activity levels and weight the VER by the proportion
-#' of breathing and speaking.  
-#' 
-#' @references 
+#' of breathing and speaking. We use the average IRs across men and women.
+#'
+#' @references
 #' - Mikszewski: Mikszewski et al. (2022) in Geoscience frontiers (doi.org/10.1016/j.gsf.2021.101285)
 #' - Adams: Adams (1993) [Report] Measurement of Breathing Rate and Volume in Routinely Performed Daily Activities.
-#' - Stadnytskyi: Stadnytskyi et al. (2020) in PNAS (doi.org/10.1073/pnas.2006874117)
+#' - Stadnytskyi: Stadnytsk yi et al. (2020) in PNAS (doi.org/10.1073/pnas.2006874117)
 #' - Morawska: Morawska et al. (2009) in Journal of Aerosol Science (doi.org/10.1016/j.jaerosci.2008.11.002)
 #' - Buonanno: Buonanno et al. (2020) in Environment International (doi.org/10.1016/j.envint.2020.106112)
 #' - Andrews: Andrews et al. (2014) in JID (doi.org/10.1093/infdis/jiu138)
 
 rq <- function(viral_load, cf = 2.0e-3, activity = "waiting", breathing_weight = .8) {
   # inhalation rates
-  IR <- c(c(7.12 + 8.98) / 2 / 1000 * 60,
-          c(7.72 + 9.30) / 2 / 1000 * 60,
-          c(8.36 + 10.65) / 2 / 1000 * 60,
-          c(20.32 + 24.13) / 2 / 1000 * 60)
+  IR <- rowMeans(IR())
   names(IR) <- c("lying (resting)", "sitting", "standing", "walking slowly (light activity)")
-  
+
   # droplet emission rates
   VER_M <- c(9.8e-4, 4.9e-3, 8.3e-2)
   names(VER_M) <- c("resting, oral breathing", "standing, speaking", "light activity, speaking loudly")
-  
-  # define 2 activity levels 
+
+  # define 2 activity levels
   if (activity == "waiting") {
-    VER <- breathing_weight * IR["sitting"] / IR["lying (resting)"] * VER_M["resting, oral breathing"] + 
-      (1-breathing_weight) * IR["sitting"] / IR["standing"] * VER_M["standing, speaking"]
+    VER <- breathing_weight * IR["sitting"] / IR["lying (resting)"] * VER_M["resting, oral breathing"] +
+      (1 - breathing_weight) * IR["sitting"] / IR["standing"] * VER_M["standing, speaking"]
   } else if (activity == "walking") {
     VER <- breathing_weight * IR["walking slowly (light activity)"] / IR["lying (resting)"] * VER_M["resting, oral breathing"] +
-      (1-breathing_weight) * IR["walking slowly (light activity)"] / IR["standing"] * VER_M["standing, speaking"]
+      (1 - breathing_weight) * IR["walking slowly (light activity)"] / IR["standing"] * VER_M["standing, speaking"]
   } else {
     stop("Error: activity undefined")
   }
-  names(VER) <- paste(activity, "with", 100*breathing_weight, "% breathing") 
-  
+  names(VER) <- paste(activity, "with", 100 * breathing_weight, "% breathing")
+
   # compute quanta emission rate
   q <- viral_load * cf * VER
-  
+
   return(q)
 }
 
 
 #' Viral load distribution
-#' 
+#'
 #' @param n number of samples
-#' 
+#'
 
 bact_load.mtb <- function(n) rlnorm(n, log(10^5.5), log(10^1.3)) # CFU mL-1
 
 #' Viral inactivation rate
-#' 
+#'
 #' @param n number of samples
-#' 
-#' @details 
+#'
+#' @details
 #' Based on a crude approximation to various estimates in the literature (see references).
-#' 
-#' @references 
+#'
+#' @references
 #' - Loudon et al. (1969) in Am Rev Respir Dis (doi.org/10.1164/arrd.1969.100.2.165)
 #' - Gannon et al. (2007) in Res J Vet Sci (doi.org/10.1016/j.rvsc.2006.07.011)
 #' - Klein and Yang (2014) in Int J Mycobateriol (doi.org/10.1016/j.ijmyco.2014.04.002)
@@ -170,22 +189,22 @@ rlambda <- function(n, disease = "TB") {
 
 
 #' Compute air change rate using transient mass balance method
-#' 
+#'
 #' @param data data frame with columns C1 (lead CO2), C (CO2), n (number of people), and V (volume)
 #' @param G CO2 generation rate in L/min
 #' @param dt time step of C and n in h
-#' 
-#' @details 
+#'
+#' @details
 #' Based on Equation 16, page 7, in Batterman assuming CO2 generation rate based G on Persily.
-#' 
-#' @references 
+#'
+#' @references
 #' - Batterman et al. (2017): https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5334699/
 #' - Persily et al. (2007): https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5666301/
-#' 
+#'
 
 transient_mass_balance <- function(A, C, n, V, Cr, G, dt) {
   Q <- A * V
-  C1hat <- 6 * 10^4 * n * G / Q * (1 - exp(- Q / V * dt)) + (C - Cr) * exp(- Q / V * dt) + Cr
+  C1hat <- 6 * 10^4 * n * G / Q * (1 - exp(-Q / V * dt)) + (C - Cr) * exp(-Q / V * dt) + Cr
   return(C1hat)
 }
 
@@ -193,18 +212,20 @@ transient_mass_balance <- function(A, C, n, V, Cr, G, dt) {
 min_rss <- function(data, par, G, dt) {
   #' par[1] is the air change rate
   #' par[2] is the outdoor CO2 level
-  with(data, sum( (C1 - transient_mass_balance(par[1], C, n, V, par[2], G, dt)) ^ 2))
+  with(data, sum((C1 - transient_mass_balance(par[1], C, n, V, par[2], G, dt))^2))
 }
 
-# optimize 
-estimate_aer <- function(data, G = 0.004 * 60, dt = 1 / 60, aer_range = c(0.01, 10, 1000), Cr_range = c(300, 450, 600)) { 
-  res <- optim(par = c(aer_range[2], Cr_range[2]), fn = min_rss, data = data, G = G, dt = dt, 
-               lower = c(aer_range[1], Cr_range[1]), upper = c(aer_range[3], Cr_range[3]), method = "L-BFGS-B")
+# optimize
+estimate_aer <- function(data, G = 0.004 * 60, dt = 1 / 60, aer_range = c(0.01, 10, 1000), Cr_range = c(300, 450, 600)) {
+  res <- optim(
+    par = c(aer_range[2], Cr_range[2]), fn = min_rss, data = data, G = G, dt = dt,
+    lower = c(aer_range[1], Cr_range[1]), upper = c(aer_range[3], Cr_range[3]), method = "L-BFGS-B"
+  )
   return(list(res))
 }
 
 #' Compute air change rate with steady-state method
-#' 
+#'
 #' @param n number of people in steady state
 #' @param G CO2 generation rate in L/min
 #' @param V volume
@@ -212,5 +233,5 @@ estimate_aer <- function(data, G = 0.004 * 60, dt = 1 / 60, aer_range = c(0.01, 
 #' @param Cr outdoor CO2 level (default 400ppm)
 
 steady_state_aer <- function(n, G, V, Cs, Cr = 400) {
-  6 * 10^4 * n * G / (V * (Cs-Cr))
+  6 * 10^4 * n * G / (V * (Cs - Cr))
 }
