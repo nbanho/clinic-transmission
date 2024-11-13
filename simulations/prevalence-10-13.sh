59d3a70b563cbb22ac55e5c0a4901387b91e044c
nbanho <nicbanjo@gmail.com>
2024-11-13 17:21:26 +0100

    modified test batch

--- simulations/prevalence-10-13.sh
+++ simulations/prevalence-10-13.sh
@@ -7,6 +7,5 @@
 #SBATCH --partition=epyc2
 
 # Your code below this line
-module load R
-
-Rscript preprocessing/simulate_qctr.R 1 10 10 13 prevalence 250 1 0 pu spattemp clinic T
+R CMD BATCH --vanilla preprocessing/simulate_qctr.R 1 10 10 13 prevalence 250 1 0 pu spattemp clinic T || exit 91
+echo "Script finished"
